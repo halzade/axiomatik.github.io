7a8e6ef3df59a903d5d694dd03d432e3687935c9
lukas <lukas@halza.de>
2026-02-04 13:13:47 +0100

    fix db

--- tests/db/test_001_db.rs
+++ tests/db/test_001_db.rs
@@ -34,46 +34,64 @@ mod tests {
     #[tokio::test]
     async fn test_create_update_read_delete() -> Result<(), database::SurrealError> {
         initialize_in_memory_database().await?;
-
-        let key = "1";
-        let table = "trivial";
-
         {
             // 1. Create
-            let w1 = database::db_write().await.unwrap();
-            let new = Trivial::new(key, "ok");
-            w1.create("trivial").content(new).await.unwrap();
+            let w1 = database::db_write().await?;
+            let new = Trivial::new("secret", "ok");
+            let created_o: Option<Trivial> = w1.create(("trivial", "1")).content(new).await?;
+
+            let created = created_o.unwrap();
+            assert_eq!(created.value, "ok");
+            assert_eq!(created.key, "secret");
         }
         {
             // 2. Read
-            let r1 = database::db_read().await.unwrap();
-            let res = r1.select(("trivial", "1")).await.unwrap();
-            let mut read: Trivial = res.unwrap();
+            let r1 = database::db_read().await?;
+            let res = r1.select(("trivial", "1")).await?;
+
+            let read_o: Option<Trivial> = res.unwrap();
+            let read = read_o.unwrap();
             assert_eq!(read.value, "ok");
-            assert_eq!(read.key, "1");
+            assert_eq!(read.key, "secret");
         }
-        // {
-        //     // 3. Update
-        //     let w2 = database::db_write().await?;
-        //     w2.update("trivial").content({}).await?;
-        // }
-        // 4. Read again
-        // let updated: Trivial = db
-        //     .read_by_key(table, key)
-        //     .await
-        //     .expect("read after update failed");
-        //
-        // assert_eq!(updated.value, "ok, updated");
-        // assert_eq!(read.key, key);
-        //
-        // // 5. Delete
-        // db.delete_struct(table, updated)
-        //     .await
-        //     .expect("delete failed");
-        //
-        // // 6. Verify deletion
-        // let deleted = db.read_by_key::<Trivial>(table, key).await;
-        // assert!(deleted.is_err());
+        {
+            // 3. Update
+            let w2 = database::db_write().await?;
+            let update_with = Trivial::new("secret 2", "ok updated");
+            let updated: Option<Trivial> = w2.update(("trivial", "1")).content(update_with).await?;
+
+            let updated = updated.unwrap();
+            assert_eq!(updated.key, "secret 2");
+            assert_eq!(updated.value, "ok updated");
+        }
+        {
+            // 4. Read again
+            let r2 = database::db_read().await?;
+            let res2 = r2.select(("trivial", "1")).await?;
+
+            let read_o2: Option<Trivial> = res2.unwrap();
+            let read2 = read_o2.unwrap();
+            assert_eq!(read2.value, "ok updated");
+            assert_eq!(read2.key, "secret 2");
+        }
+        {
+            // 5. Delete
+            let w3 = database::db_write().await?;
+            let deleted: Option<Trivial> = w3
+                .delete(("trivial", "1")) // specify record ID
+                .await?;
+
+            let deleted = deleted.unwrap();
+            assert_eq!(deleted.key, "secret 2"); // or whatever the key was before
+            assert_eq!(deleted.value, "ok updated");
+        }
+        {
+            // 6. Verify deletion
+            let r3 = database::db_read().await?;
+            let res3: Option<Trivial> = r3.select(("trivial", "1")).await?;
+            // was deleted
+            assert!(res3.is_none());
+        }
         Ok(())
     }
 }
